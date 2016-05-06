385c972dfec1fd74ae7efe655724548f9b9379f6
Lara Rode <lara.rode@hotmail.com>
2016-05-06 13:00:35 -0700

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -14,21 +15,15 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET
 
 app.get('/todos', function (req, res){
 	res.json(todos);
 });
 
+
 app.get('/todos/:id', function (req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todo.id === todoId){
-			matchedTodo = todo;
-		}
-	});
+	var matcedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo){
 		res.json(matchedTodo);
@@ -41,7 +36,13 @@ app.get('/todos/:id', function (req, res){
 
 
 app.post( '/todos', function (req, res) {
-	var body = req.body;
+	var body =  _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 
 	body.id = todoNextId++;
 	todos.push(body);
@@ -53,3 +54,5 @@ app.post( '/todos', function (req, res) {
 app.listen(PORT, function () {
 	console.log('Express listening on port: ' + PORT + '!');
 });
+
+
