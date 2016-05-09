d8e3ac529e19ac13a8c9c00e6280f1faf0d96b36
Lara Rode <lara.rode@hotmail.com>
2016-05-09 10:52:38 -0700

    Add login route and authenticate class method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -14,26 +15,28 @@ app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos?completed=true&q=house
+// GET /todos?completed=false&q=work
 app.get('/todos', function(req, res) {
 	var query = req.query;
 	var where = {};
 
-	if (query.hasOwnProperty('completed') && query.completed === 'true'){
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
-	}
-	else if (query.hasOwnProperty('completed') && query.completed === 'false'){
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
 		where.completed = false;
 	}
 
-	if (query.hasOwnProperty('q') && query.q.length > 0){
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
 		where.description = {
 			$like: '%' + query.q + '%'
 		};
 	}
-	db.todo.findAll({where: where}).then(function (todos){
+
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		res.json(todos);
-	}, function (e){
+	}, function(e) {
 		res.status(500).send();
 	});
 });
@@ -42,14 +45,13 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function (todo) {
-		if (!!todo){
+	db.todo.findById(todoId).then(function(todo) {
+		if (!!todo) {
 			res.json(todo.toJSON());
-		}
-		else{
+		} else {
 			res.status(404).send();
 		}
-	}, function (e){
+	}, function(e) {
 		res.status(500).send();
 	});
 });
@@ -58,12 +60,11 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body).then(function (todo){
+	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	}, function (e) {
+	}, function(e) {
 		res.status(400).json(e);
 	});
-
 });
 
 // DELETE /todos/:id
@@ -74,15 +75,15 @@ app.delete('/todos/:id', function(req, res) {
 		where: {
 			id: todoId
 		}
-	}).then(function (rowsDeleted){
-		if(rowsDeleted === 0){
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0) {
 			res.status(404).json({
-				error: "No todo with id"
+				error: 'No todo with id'
 			});
-		}else {
+		} else {
 			res.status(204).send();
 		}
-	}, function (e){
+	}, function() {
 		res.status(500).send();
 	});
 });
@@ -93,7 +94,7 @@ app.put('/todos/:id', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	var attributes = {};
 
-	if (body.hasOwnProperty('completed') ) {
+	if (body.hasOwnProperty('completed')) {
 		attributes.completed = body.completed;
 	}
 
@@ -101,38 +102,46 @@ app.put('/todos/:id', function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function (todo){
-		if (todo){
-			todo.update(attributes).then(function (todo) {
-			res.json(todo.toJSON());
-		}, function (e) {
-			res.status(404).json(e);
-		});
-		}else {
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
+			todo.update(attributes).then(function(todo) {
+				res.json(todo.toJSON());
+			}, function(e) {
+				res.status(400).json(e);
+			});
+		} else {
 			res.status(404).send();
 		}
-	}, function (){
+	}, function() {
 		res.status(500).send();
 	});
 });
 
-
-// POST /users
-app.post('/users', function(req, res) {
+app.post('/users', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
-	db.todo.create(body).then(function (user){
+	db.user.create(body).then(function (user) {
 		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(400).json(e);
 	});
 });
 
+// POST /users/login
+app.post('/users/login', function (req, res) {
+	var body = _.pick(req.body, 'email', 'password');
 
-db.sequelize.sync().then(function () {
-	app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + '!');
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function (e) {
+		res.send(401).send();
+	});
 });
 
 
+
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
